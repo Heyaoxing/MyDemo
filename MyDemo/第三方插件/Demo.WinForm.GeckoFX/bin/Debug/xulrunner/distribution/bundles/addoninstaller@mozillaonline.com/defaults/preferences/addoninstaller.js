0553d75fedf1be74d7becd9b0691bb9974c51319
730530507 <730530507@qq.com>
2016-08-03 10:16:09 +0800

    修改了存放目录

--- MyDemo/第三方插件/Demo.WinForm.GeckoFX/bin/Debug/xulrunner/distribution/bundles/addoninstaller@mozillaonline.com/defaults/preferences/addoninstaller.js
+++
@@ -1,6 +0,0 @@
-/**
- * Copyright (c) 2010 Appcoast Ltd. All rights reserved.
- */
-
-// global preference to record completion time of the process
-pref("extensions.addoninstaller.installation.completedtime", '0');
