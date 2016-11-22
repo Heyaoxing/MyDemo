867b347041bf8cb6e4b4a9133b62525925953726
730530507 <730530507@qq.com>
2016-11-22 16:17:39 +0800

    增加公共类

---
+++ MyDemo/Net编程/Demo.Net.Infrastructure/InternetHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Net.Infrastructure
+{
+    public class InternetHelper
+    {
+        //获取内网IP
+        private static string GetInternalIP()
+        {
+            IPHostEntry host;
+            string localIP = "000";
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily.ToString() == "InterNetwork")
+                {
+                    localIP = ip.ToString();
+                    break;
+                }
+            }
+            return localIP;
+        }
+    }
+}
---
+++ MyDemo/Net编程/Demo.Net.Infrastructure/TextHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Net.Infrastructure
+{
+    public class TextHelper
+    {
+        /// <summary>
+        /// 过滤数字
+        /// </summary>
+        /// <param name="txt"></param>
+        /// <returns></returns>
+        public static string FilterNumber(string txt)
+        {
+            if (string.IsNullOrWhiteSpace(txt))
+                return txt;
+
+            return System.Text.RegularExpressions.Regex.Replace(txt, @"\d", "");
+        }
+
+
+        /// <summary>
+        /// 过滤英文
+        /// </summary>
+        /// <param name="txt"></param>
+        /// <returns></returns>
+        public static string FilterEnglish(string txt)
+        {
+            if (string.IsNullOrWhiteSpace(txt))
+                return txt;
+
+            return System.Text.RegularExpressions.Regex.Replace(txt, @"[a-zA-Z]", "");
+        }
+    }
+}
